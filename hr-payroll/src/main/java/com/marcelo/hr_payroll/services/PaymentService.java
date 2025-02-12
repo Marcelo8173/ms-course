10c7dd861fb68f58cdc11d8841dbc63bb10725f2
Marcelo andre <marceloandrebio@gmail.com>
2025-02-12 17:34:15 -0300

    restController

--- hr-payroll/src/main/java/com/marcelo/hr_payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/marcelo/hr_payroll/services/PaymentService.java
@@ -1,13 +1,31 @@
 package com.marcelo.hr_payroll.services;
 
 import com.marcelo.hr_payroll.entities.Payment;
+import com.marcelo.hr_payroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(Long workerId, Integer days) {
-        Payment payment = new Payment("marcelo", 200.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", ""+workerId);
+        System.out.println(workerHost);
+        Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class, uriVariables);
+
+        Payment payment = new Payment(worker.getName(), worker.getDailyIncome(), days);
         return payment;
     }
 }
