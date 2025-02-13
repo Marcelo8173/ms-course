bc90bd8f51c47459745e526eab23d74464ecda43
Marcelo andre <marceloandrebio@gmail.com>
2025-02-13 20:20:54 -0300

    load balance

--- hr-worker/src/main/java/com/marcelo/hr_worker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/marcelo/hr_worker/controller/WorkerController.java
@@ -3,7 +3,10 @@ package com.marcelo.hr_worker.controller;
 import com.marcelo.hr_worker.entities.Worker;
 import com.marcelo.hr_worker.repositories.WorkerRepository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +20,11 @@ import java.util.List;
 @RequestMapping("/workers")
 public class WorkerController {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+
+    @Autowired
+    Environment environment;
+
     @Autowired
     private WorkerRepository workerRepository;
 
@@ -28,6 +36,9 @@ public class WorkerController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Worker> listWorker (@PathVariable Long id) {
+
+        logger.info("port12=" + environment.getProperty("local.server.port"));
+
         Worker worker = this.workerRepository.findById(id).get();
         return new ResponseEntity<>(worker, HttpStatus.OK);
     }
