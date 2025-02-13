3a2de0d1c7dcc15e49a22def608e377394ac2486
Marcelo andre <marceloandrebio@gmail.com>
2025-02-13 19:59:11 -0300

    chamadas co feing client

--- hr-payroll/src/main/java/com/marcelo/hr_payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/marcelo/hr_payroll/services/PaymentService.java
@@ -2,10 +2,10 @@ package com.marcelo.hr_payroll.services;
 
 import com.marcelo.hr_payroll.entities.Payment;
 import com.marcelo.hr_payroll.entities.Worker;
+import com.marcelo.hr_payroll.feingClients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,17 +13,14 @@ import java.util.Map;
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
+//    @Value("${hr-worker.host}")
+//    private String workerHost;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(Long workerId, Integer days) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", ""+workerId);
-        System.out.println(workerHost);
-        Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class, uriVariables);
+        Worker worker = this.workerFeignClient.listWorker(workerId).getBody();
 
         Payment payment = new Payment(worker.getName(), worker.getDailyIncome(), days);
         return payment;
